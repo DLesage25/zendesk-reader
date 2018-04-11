3f8ca81eba38ea86a67ebedbbd83ebaefdf0bbb5
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-04-11 12:21:23 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import BookList from '../containers/book-list';
+
+export default class App extends Component {
+  render() {
+    return (
+    	<div>
+    		<div >
+      				<BookList />
+      		</div>
+      	</div>
+    );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'Javascript'},
+		{title: 'Node'},
+		{title: 'Ruby'},
+		{title: 'C++'}
+	]
+}
