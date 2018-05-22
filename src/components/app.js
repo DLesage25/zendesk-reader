5974227c8d40da27c379eab3a92a6bbffd89041d
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-05-21 19:45:56 -0600

    adding new styles

--- src/components/app.js
+++ src/components/app.js
@@ -5,11 +5,32 @@ import BookList from '../containers/book-list';
 export default class App extends Component {
   render() {
     return (
-    	<div>
-    		<div >
-      				<BookList />
-      		</div>
-      	</div>
+    	<div className="container-fluid">
+	    	<div style = {{ maxWidth : '1400px', maxHeight : '100%' }}>
+		            <nav className = 'top-menu'>
+		                <div className = 'menu-icon-container hidden-md-up'>
+		                    <div className = 'animate-menu-button left-menu-toggle'>
+		                        <div />
+		                    </div>
+		                </div>
+						this is a test
+		            </nav>
+		    <section className = 'page-content'>
+                <div className = 'page-content-inner'>
+                    <nav className = 'top-submenu top-submenu-with-background'>
+                        <div className = 'row' style = {{ height:'100%'}} >
+				    <div className="col-3">
+				      One of three columns
+				    </div>
+				    <div className="col-md">
+				      One of three columns
+				    </div>
+				  </div>
+				  </nav>
+				  </div>
+			</section>
+			  </div>
+		  </div>
     );
   }
 }
