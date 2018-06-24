e2f8231adccbac1634d3b5aa6ae34f1db26764f4
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-06-24 14:22:18 -0600

    adding productivity data

---
+++ src/actions/index.js
@@ -0,0 +1,101 @@
+import dataTypes from '../components/dataTypes';
+import FB from 'firebase';
+
+let AccessData = {
+    apiKey: "AIzaSyA_7XYTtYjyMedZ8_TZhspDlOERSBKc4pE",
+    authDomain: "zendeskreader.firebaseapp.com",
+    databaseURL: "https://zendeskreader.firebaseio.com",
+    projectId: "zendeskreader",
+    storageBucket: "zendeskreader.appspot.com",
+    messagingSenderId: "45174707150"
+};
+FB.initializeApp(AccessData);
+const DB     = FB.database();
+
+async function get(path){
+    err.isInvalidPath(arguments,'path',path);
+    let res = await DB.ref('/' + path).once('value');
+    if(res) res = res.val();
+    return res;
+};
+
+function write(path,data){
+    err.isInvalidPath(arguments,'path',path);
+    err.isInvalidWriteData(arguments,'data',data);
+    DB.ref('/' + path).set(data);
+    return true;
+};
+
+function remove(path){
+    err.isInvalidPath(arguments,'path',path);
+    DB.ref('/' + path).remove();
+    return true;
+};
+
+function unwatch(path){
+    err.isInvalidPath(arguments,'path',path);
+    DB.ref('/' + path).off();
+    return true;
+};
+
+var provider = new FB.auth.GoogleAuthProvider();
+provider.setCustomParameters({ 'hd': 'partnerhero.com' });
+
+export async function login(){
+    if(localStorage.getItem('running')===null){
+        try{
+            localStorage.setItem('running',true);
+            FB.auth().signInWithRedirect(provider);
+            return dispatch => { return dispatch({ type: 'NOTHING', payload: '' });  };
+        } catch (error){
+            const { errorCode, errorMessage, email, credential } = error;
+            console.error(error);
+        }
+    } else {
+        try{
+            localStorage.removeItem('running');
+            const data = await FB.auth().getRedirectResult();
+            const user = { 
+                ...data.additionalUserInfo.profile, 
+                ...data.credential
+            };
+            let accessData = user.email;
+
+            return dispatch => { return dispatch({ type: SET_ENTRY, payload: accessData });  };
+        } catch(error){
+            console.error(error);
+            localStorage.removeItem('running');
+            return dispatch => { return dispatch({ type: SET_ENTRY, payload: '' });  };
+        }
+    }
+};
+
+export function fetchAndInitialize(email){
+    return async dispatch => {
+        const userID = email2id(email);
+        let [
+            userData,
+            //settings,
+            productionData
+        ] = await Promise.all([
+            get('users/current/byUserID/' + userID),
+            //get('settings/byUser/' + userID),
+            get(program + '/production')
+        ]);
+
+        if(!userSettings){
+            const newSett = newSettings();
+            userSettings = newSett;
+            postSettings2(userID,newSett);
+        }
+
+        const prettyObject = {
+            user : {
+                ...userData,
+                userSettings         : userSettings,
+            },
+            productionData          : productionData
+        };
+        return dispatch({ type: FETCH_ALL_DATA, payload: prettyObject });
+    }
+};
--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,75 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect }            from 'react-redux';
+import { 
+    login,
+    fetchAndInitialize
+}                             from '../actions';
+// import { 
+//     login,
+//     fetchAndInitialize
+// }                             from '../actions';
+// import Signed                 from './signed';
+// import ThingyLoader           from './thingyloader';
 
 import Generalpage from './generalPage';
 
 import TopBar from './topBar';
 
 export default class App extends Component {
-  render() {
-    return (
-    	<div className="container-fluid">
-	    	<div style = {{ maxWidth : '1400px', maxHeight : '100%' }}>
-	    	<TopBar />
-		    <section className = 'page-content'>
-                <div className = 'page-content-inner'>
-                    <nav className = 'top-submenu top-submenu-with-background' />
-                    <Generalpage />
-			  	</div>
-			</section>
-			  </div>
-		  </div>
-    );
+  constructor(props){
+    super(props);
+    if(!this.props.EntryData) this.props.login();
   }
+
+  render() {
+        const { EntryData, Loaded } = this.props;
+        if(!EntryData) return <div />;
+        return(
+            <div>
+                { 
+                    !Loaded ?
+                    <div style = {{ maxWidth:'1400px' }}>
+                        <div 
+                            style = {{ 
+                                position:   'absolute',
+                                top:        '50%',
+                                left:       '50%',
+                                transform:  'translate(-50%,-50%)'
+                            }}
+                        >
+                            <p>
+                                Loading...
+                            </p>
+                        </div>
+                    </div>
+                    : <Signed />
+                }
+            </div>
+        );
+  }
+
+    componentDidUpdate(){
+        if(this.props.EntryData && !this.props.Loaded)
+        this.props.fetchAndInitialize(this.props.EntryData);
+    }
 }
+
+function mapDispatchToProps(dispath){
+    return bindActionCreators({
+        login : login,
+        fetchAndInitialize : fetchAndInitialize
+    }, dispath);
+}
+
+function mapStateToProps(state){
+    const {
+
+    } = state;
+    return { 
+        EntryData : entryData,
+        Loaded    : 'rubricData && userData && userAssessments'
+    };
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
