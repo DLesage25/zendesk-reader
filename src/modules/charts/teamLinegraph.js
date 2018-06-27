92eb02c4ae2b1c41a5064d074145885d3b07b850
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-06-27 09:08:45 -0600

    adding json loader

--- src/modules/charts/teamLinegraph.js
+++ src/modules/charts/teamLinegraph.js
@@ -2,26 +2,31 @@ import _ from 'lodash';
 import data from './chartdata';
 
 const formatChartData = async (chartData) => {
-		let formattedData = await processDays(chartData);
+		let formattedData = await processAllDays(chartData);
 		return formattedData;
 }
 
-const processDays = async (chartData) => {
+const processAllDays = async (chartData) => {
 		let days = Object.keys(chartData);
 		let formattedData = [];
 
-		days.map(async (day) => {
-			let dayData = chartData[day];
-			let formattedDayData = await getGoalLine(dayData)
-			formattedData.push(formattedDayData) 
+		days.map(async (dayKey) => {
+			let dayData = chartData[dayKey];
+			let formattedDayData = await buildDayObject(dayKey, dayData)
+			formattedData.push({[dayKey]:formattedDayData}) 
 		})
-
 		return formattedData;
 }
 
-const getGoalLine = async (dayData) => {
-		let hours = Object.keys(dayData);
+const buildDayObject = async (dayKey, dayData) => {	
+	let hours = Object.keys(dayData);
+	let dayObject = await getSeries(dayData, hours);
+	return dayObject;
+}
+
+const getSeries = async (dayData, hours) => {
 		let goalLine = [];
+		let productionLine = [];
 
 		hours.map((hour)=>{
 			let index = dayData[hour];
@@ -32,11 +37,13 @@ const getGoalLine = async (dayData) => {
 			index = _.filter(index, function(o){ return o.email })
 
 			let totalHourlyGoal = _.sumBy(index, function(o) { return Number(o.goal) });
+			let totalHourlyProd = _.sumBy(index, function(o) { return Number(o.touches) });
 
 			goalLine.push(totalHourlyGoal);
+			productionLine.push(totalHourlyProd);
 		})
 
-		return goalLine
+		return {goalLine: goalLine, productionLine: productionLine, hourLine: hours}
 }
 
 export default {
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 module.exports = {
-  entry: [
-    'babel-polyfill',
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-        rules : [
-        {
-        exclude : /node_modules/,
-        loader  : 'babel-loader',
-        query   : { 
-            presets : [
-                'react',
-                'babel-preset-env'
-            ] 
-        }
-        }
+    entry: [
+        'babel-polyfill',
+        './src/index.js'
     ],
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  },
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    'react',
+                    'babel-preset-env'
+                ]
+            }
+        }],
+        loaders: [{
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-1']
+                }
+            },
+            {
+                test: /\.json$/,
+                // loader: 'json'
+                loader: 'json-loader' // Change this
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: './'
+    },
     node: {
         fs: 'empty',
         net: 'empty',
