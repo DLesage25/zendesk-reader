b87c20c2e5d023916cc766b271e65de455010b30
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-07-08 21:52:29 -0600

    formatting queue data and adding additional color schemes

--- src/modules/charts/linegraph/colorScheme.js
+++ src/modules/charts/linegraph/colorScheme.js
@@ -12,14 +12,59 @@ const colorSchemes = [{
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
         pointHighlightStroke: "rgba(151,187,205,1)"
-    },
-    {
+    }, {
         fillColor: "rgb(106,168,148, 0.2)",
         strokeColor: "#05583e",
         pointColor: "#05583e",
         pointStrokeColor: "#fff",
         pointHighlightFill: "#fff",
         pointHighlightStroke: "rgba(151,187,205,1)"
+    }, {
+        fillColor: "rgba(217, 30, 24,0.2)",
+        strokeColor: "rgba(217, 30, 24,1)",
+        pointColor: "rgba(217, 30, 24,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(217, 30, 24,1)"
+    }, {
+        fillColor: "rgba(249, 191, 59,0.2)",
+        strokeColor: "rgba(249, 191, 59,1)",
+        pointColor: "rgba(249, 191, 59,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(249, 191, 59,1)"
+    },
+    {
+        fillColor: "rgba(1, 50, 67, 0.2)",
+        strokeColor: "rgba(1, 50, 67, 1)",
+        pointColor: "rgba(1, 50, 67, 1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(1, 50, 67, 1)"
+    },
+    {
+        fillColor: "rgba(224, 130, 131, 0.2)",
+        strokeColor: "rgba(224, 130, 131, 1)",
+        pointColor: "rgba(224, 130, 131, 1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(224, 130, 131, 1)"
+    },
+    {
+        fillColor: "rgba(102, 51, 153, 0.2)",
+        strokeColor: "rgba(102, 51, 153, 1)",
+        pointColor: "rgba(102, 51, 153, 1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(102, 51, 153, 1)" 
+    },
+    {
+        fillColor: "rgba(144, 198, 149, 0.2)",
+        strokeColor: "rgba(144, 198, 149, 1)",
+        pointColor: "rgba(144, 198, 149, 1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(144, 198, 149, 1)"
     }
 ]
 
---
+++ src/modules/charts/linegraph/formatQueueData.js
@@ -0,0 +1,90 @@
+import _ from 'lodash';
+import colorSchemes from './colorScheme';
+
+const formatChartData = async(programData) => {
+    let groupedData = await groupAllData(programData);
+    let formattedData = await formatAllData(groupedData);
+    return formattedData;
+}
+
+const groupAllData = async(programData) => {
+    let groupedData = await processAllDays(programData);
+    return groupedData;
+}
+
+const formatAllData = async(groupedData) => {
+    return groupedData.map((dayIndex, dayKey) => {
+        var series = dayIndex.series;
+        var seriesKeys = Object.keys(series);
+        var data = {
+            dayKey: dayIndex.dayKey,
+            labels: dayIndex.hourLine,
+            datasets: seriesKeys.map((seriesKey, arrayKey) => {
+                return {
+                    ...colorSchemes[arrayKey],
+                    label: seriesKey,
+                    data: series[seriesKey]
+                }
+            })
+        };
+        return data;
+    })
+}
+
+const processAllDays = async(programData) => {
+	let productivity = programData.productivity;
+    let days = Object.keys(productivity);
+    let groupedData = [];
+
+    days.map(async(dayKey) => {
+        let dayData = productivity[dayKey];
+        let formattedDayData = await buildDayObject(dayKey, dayData)
+        groupedData.push(formattedDayData)
+    })
+    return groupedData;
+}
+
+const buildDayObject = async(dayKey, dayData) => {
+    let hours = Object.keys(dayData);
+    let dayObject = await getSeries(dayData, hours, dayKey);
+    return dayObject;
+}
+
+const getSeries = async(dayData, hours, dayKey, goal) => {
+    let queueLoads = {};
+
+    hours.map((hour) => {
+        let index = dayData[hour];
+
+        //convert to array
+        index = Object.keys(index).map(function(key) { return index[key]; });
+
+        //filter out queue load
+        index = _.find(index, function(o) { return o.type === 'queueLoad' });
+
+        if (!index) return true;
+
+        let queueNames = Object.keys(index);
+
+        queueNames.map((queueName) => {
+            let queueLoad = index[queueName];
+
+            if (queueLoad === 'NaN') queueLoad = null;
+
+            if(!queueLoads[queueName]) queueLoads[queueName] = [];
+            queueLoads[queueName].push(queueLoad)
+        })
+    })
+
+    return {
+        dayKey: dayKey,
+        hourLine: hours,
+        series: {
+            ...queueLoads
+        }
+    }
+}
+
+export default {
+    formatChartData: formatChartData
+}
