bcb21187c63c6acee71caf3dd67c79defed34a47
Daniel Le'Sage <d_lesage3@hotmail.com>
2018-07-18 19:59:34 -0600

    adding settings to new partners

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,15 @@ import dataTypes from '../components/dataTypes';
 import FB from 'firebase';
 import rest from 'restler';
 import moment from 'moment-timezone';
+import _ from 'lodash';
+
+import newSettings from '../modules/newSettings';
 
 import formatTeamLinegraphData from '../modules/charts/linegraph/formatTeamData';
 import formatIndividualLinegraphData from '../modules/charts/linegraph/formatIndividualData';
 import formatQueueData from '../modules/charts/linegraph/formatQueueData';
 
+import tempBambooData from './tempBambooData';
 
 const {
     FETCH_ALL_DATA,
@@ -25,69 +29,126 @@ let AccessData = {
     storageBucket: "zendeskreader.appspot.com",
     messagingSenderId: "45174707150"
 };
-FB.initializeApp(AccessData);
-const DB     = FB.database();
 
-async function get(path){
+let BambooOptions = {
+    username: 'b2bb8e562b200aa67bfcbbc6b1fce3c9685e7b59',
+    password: 'x',
+    headers: {
+        Accept: 'application/json'
+    }
+};
+
+FB.initializeApp(AccessData);
+const DB = FB.database();
+
+async function get(path) {
     let res = await DB.ref('/' + path).once('value');
-    if(res) res = res.val();
+    if (res) res = res.val();
     return res;
 };
 
-function write(path,data){
+function write(path, data) {
     DB.ref('/' + path).set(data);
     return true;
 };
 
-function remove(path){
+function remove(path) {
     DB.ref('/' + path).remove();
     return true;
 };
 
-function unwatch(path){
+function unwatch(path) {
     DB.ref('/' + path).off();
     return true;
 };
 
-export function email2id   ( email ) { return email.substring(0,email.indexOf('@')).replace(/\./g,'_'); };
+export function email2id(email) { return email.substring(0, email.indexOf('@')).replace(/\./g, '_'); };
 
 var provider = new FB.auth.GoogleAuthProvider();
 provider.setCustomParameters({ 'hd': 'partnerhero.com' });
 
-export async function login(){
-    if(localStorage.getItem('running')===null){
-        try{
-            localStorage.setItem('running',true);
+export async function login() {
+    if (localStorage.getItem('running') === null) {
+        try {
+            localStorage.setItem('running', true);
             FB.auth().signInWithRedirect(provider);
-            return dispatch => { return dispatch({ type: 'NOTHING', payload: '' });  };
-        } catch (error){
+            return dispatch => { return dispatch({ type: 'NOTHING', payload: '' }); };
+        } catch (error) {
             const { errorCode, errorMessage, email, credential } = error;
             console.error(error);
         }
     } else {
-        try{
+        try {
             localStorage.removeItem('running');
             const data = await FB.auth().getRedirectResult();
-            const user = { 
-                ...data.additionalUserInfo.profile, 
+            const user = {
+                ...data.additionalUserInfo.profile,
                 ...data.credential
             };
             let accessData = user.email;
 
-            return dispatch => { return dispatch({ type: SET_ENTRY, payload: accessData });  };
-        } catch(error){
+            return dispatch => { return dispatch({ type: SET_ENTRY, payload: accessData }); };
+        } catch (error) {
             console.error(error);
             localStorage.removeItem('running');
-            return dispatch => { return dispatch({ type: SET_ENTRY, payload: '' });  };
+            return dispatch => { return dispatch({ type: SET_ENTRY, payload: '' }); };
         }
     }
 };
 
-export function fetchUserData(email){
+// post settings
+
+export function postProgramSettings(program, settings) {
+    write(program + '/settings', settings);
+}
+
+// export function getProgramBambooId(program) {
+//     if()
+// }
+
+//the getprogram function should pull all users from /users in FB and filter using that
+export async function getProgramRoster(program) {
+    let users = await get('/users/byUserId');
+
+    return _.filter(users, function(o){ return o.program.toLowerCase() === program })
+}
+
+//leave this for prod, ut fixed o pull from actual bamboo api
+export async function syncProgramRoster(programData) {
+
+    //this is not working due to CORS, so using local file
+    // rest.get('https://api.bamboohr.com/api/gateway.php/partnerhero/v1/reports/232', BambooOptions).on('complete', (result) => {
+    //     console.log(result)
+    // })
+    // //filter for users without an email and department
+    // _.map(result.employees, (index) => {
+
+    // });
+
+    let bambooData = tempBambooData;
+
+    let userTree = {};
+
+    bambooData.map((index) => {
+        console.log(index)
+        let userId = email2id(index.workEmail)
+        userTree[userId] = {
+            email: index.workEmail,
+            firstName: index.firstName,
+            lastName: index.lastName,
+            program: index.department
+        }
+    })
+
+    write('/users/byUserId', userTree);
+}
+
+export function fetchUserData(email) {
     return dispatch => {
         DB.ref('/users').once('value', snapshot => {
-            dispatch({ type: FETCH_USER_DATA, payload: [snapshot.val(),email] });
-    });};
+            dispatch({ type: FETCH_USER_DATA, payload: [snapshot.val(), email] });
+        });
+    };
 };
 
 export function getLinegraphData(programData) {
@@ -108,38 +169,50 @@ export function getLinegraphData(programData) {
             queueData
         }
 
-        console.log({queueData})
+        console.log({ queueData })
 
         return dispatch({ type: GRAPH_DATA, payload: payload });
     }
 }
 
-export function fetchAndInitialize(email){
+export function fetchAndInitialize(email) {
     return async dispatch => {
         const userID = email2id(email);
         console.log(userID);
+        const program = 'grindr'
         let [
             userData,
             //settings,
             programData
         ] = await Promise.all([
             get('users/byUserId/' + userID),
-            get('khan/') //fix this
-            //get(program + '/production')
+            get(program + '/')
         ]);
 
-        // if(!userSettings){
-        //     const newSett = newSettings();
-        //     userSettings = newSett;
-        //     postSettings2(userID,newSett);
-        // }
+        console.log({userData}, {programData})
+
+        // let userData = await get('users/byUserId/' + userID);
+        // let programData = await get(programName + '/');
+
+        //1. I need a way of determining the program(s) a user is in/ has access to
+        //2. I need to check if program has settings, and if not include them
+
+        if (!programData.settings) {
+            console.log('no settings')
+            //add a team then add to settings
+            const team = await getProgramRoster(program);
+            const settings = newSettings.program(program);
+            settings.team = team;
+            programData.settings = settings;
+            postProgramSettings(program, settings);
+        }
 
         const prettyObject = {
-            userData : {
+            userData: {
                 ...userData
                 //userSettings         : userSettings,
             },
-            programData          : programData,
+            programData: programData,
             globalDate: moment().format('MM_DD_YY')
         };
 
