08dfb770eb3d394e1a9e88dae6be6bbdac18a750
Daniel Lesage <d_lesage3@hotmail.com>
2018-07-22 22:32:50 -0600

    removing logs

--- src/reducers/globalDate.js
+++ src/reducers/globalDate.js
@@ -1,7 +1,6 @@
 export default (state = null, action) => {
     switch (action.type) {
         case 'CHANGE_GLOBAL_DATE':
-        console.log('CHANGE_GLOBAL_DATE red', {action})
             return {
                 ...state,
                 globalDate: action.payload
