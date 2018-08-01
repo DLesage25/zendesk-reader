ccd5369f01f4624adf71d6b353d6ab337171deb2
Daniel Lesage <d_lesage3@hotmail.com>
2018-08-01 14:10:39 -0600

    fixing top bar scroll css transition

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import Generalpage          from './generalPage';
 import TopBar               from './topBar';
 import ScrollDetector from './ScrollDetector';
