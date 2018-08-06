695a1b442fe1fe582e65be4fcd4f5d40ebf24f71
Daniel Lesage <d_lesage3@hotmail.com>
2018-08-05 18:00:31 -0600

    minor styling fixes

--- src/components/productivityCard.js
+++ src/components/productivityCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 import CardComponent from './cardComponent'
 import Dropdown from './dropdown'
@@ -30,8 +31,13 @@ export default class ProductivityCard extends Component {
 				<section className="card">
 				    <div className="card-header">
 				        <span className="cat__core__title card-title" style={{ fontSize: '18px' }}>
-				            <Dropdown current={globalProgram.settings.prettyName} options={programList} action={changeGlobalProgram} buttonClassName="btn btn-sm btn-primary ml-2 dropdown-toggle" />
-				            <Dropdown current={globalDate} options={dateList} action={changeGlobalDate} buttonClassName="btn btn-sm btn-secondary ml-2 dropdown-toggle"/>
+				        	<div style={{ float: 'left'}}>
+					            <Dropdown current={globalProgram.settings.prettyName} options={programList} action={changeGlobalProgram} buttonClassName="btn btn-sm btn-primary ml-2 dropdown-toggle" />
+					            <Dropdown current={globalDate} options={dateList} action={changeGlobalDate} buttonClassName="btn btn-sm btn-secondary ml-2 dropdown-toggle"/>
+				            </div>
+				            <div style={{ float: 'right', marginRight: '100px'}}>
+				            	<span style={{fontSize:'13px', color:'gray'}}> <b>Last updated</b> { moment().format('MM/DD @ hh:00a')} </span>
+				            </div>
 				        </span>
 				    </div>
 				    <div className="card-body">
