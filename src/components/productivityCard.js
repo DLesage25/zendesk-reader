1392fa77e2fb8a6497e303e4a764cd9582c13c86
Daniel Lesage <d_lesage3@hotmail.com>
2018-08-08 09:53:25 -0600

    changing productivity card button styling

--- src/components/productivityCard.js
+++ src/components/productivityCard.js
@@ -33,12 +33,12 @@ export default class ProductivityCard extends Component {
 				    <div className="card-header">
 				        <span className="cat__core__title card-title" style={{ fontSize: '18px' }}>
 				        	<div style={{ float: 'left'}}>
-					            <Dropdown current={globalProgram.settings.prettyName} options={programList} action={changeGlobalProgram} buttonClassName="btn btn-sm btn-primary ml-2 dropdown-toggle" />
-					            <Dropdown current={globalDate} options={dateList} action={changeGlobalDate} buttonClassName="btn btn-sm btn-secondary ml-2 dropdown-toggle btn-secondary-light"/>
+					            <Dropdown current={globalProgram.settings.prettyName} options={programList} action={changeGlobalProgram} buttonClassName="btn btn-sm btn-outline-primary ml-2 dropdown-toggle" />
+					            <Dropdown current={globalDate} options={dateList} action={changeGlobalDate} buttonClassName="btn btn-sm btn-outline-secondary ml-2 dropdown-toggle"/>
 				            </div>
 				            <div style={{ float: 'right', marginRight: '10px'}}>
 				            	<span style={{fontSize:'13px', color:'gray'}}> <b>Last updated</b> { (lastFetch) ? moment(lastFetch, 'X').format('MM/DD @ hh:mma') : moment().format('MM/DD @ hh:00a') } </span>
-							<a className="btn btn-sm ml-2 btn-primary" onClick={() => this.props.refreshData()} > <i className="fa fa-sync-alt" style={{ color:'white' }}/> </a>
+							<button type="button" className="btn btn-sm ml-2 btn-outline-primary" onClick={() => this.props.refreshData()} > <i className="fa fa-sync-alt" style={{ color:"#007bff" }}/> </button>
 				            </div>
 				        </span>
 				    </div>
