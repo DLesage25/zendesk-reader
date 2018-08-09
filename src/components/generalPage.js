d1ae36de494255e98aa82a73f350cecd82bd41d0
Daniel Lesage <d_lesage3@hotmail.com>
2018-08-09 10:50:31 -0600

    styling changes

--- src/components/generalPage.js
+++ src/components/generalPage.js
@@ -123,12 +123,12 @@ class Generalpage extends Component {
                                 lastFetch           = {lastFetch} 
                             >
 
-                                <h4 className="card-body-title"> Team </h4> 
+                                <h4 className="card-body-title"> TEAM </h4> 
     			    		   { !GraphData ? <p> Loading </p> : <TeamLinegraphRenderer 
                                                                     TeamGraphData = {GraphData} 
                                                                     globalDate    = {appData.globalDate} /> }
                                 <hr />
-                                <h4 className="card-body-title"> Individual </h4>
+                                <h4 className="card-body-title"> INDIVIDUAL </h4>
     			    		   { !GraphData ? <p> Loading </p> : <IndividualLinegraphRenderer 
                                                                     IndividualGraphData = {GraphData.individualGraphData} 
                                                                     globalDate         = {appData.globalDate}/> }
