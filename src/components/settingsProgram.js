952ccdf4fa0df4db371217618e2ba1cfc9a8be89
Andres Umanzor <andres.umanzor@hotmail.com>
2018-08-13 09:15:14 -0600

    adding TextInput component

--- src/components/settingsProgram.js
+++ src/components/settingsProgram.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TextInput from './textInput';
 
 export default class SettingsProgram extends Component {
     constructor(props){
@@ -13,30 +14,10 @@ export default class SettingsProgram extends Component {
 
 		return (
 			<div className="col-md">
-			    <label htmlFor="basic-url">Program Name</label>
-                <div className="input-group mb-3">
-                  <input type="text" className="form-control" placeholder="Acme corp" aria-label="Username" aria-describedby="basic-addon1" />
-                </div>
-                <label htmlFor="basic-url">Zendesk URL</label>
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text" id="basic-addon3">https://</span>
-                  </div>
-                  <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                  <div className="input-group-append">
-                    <span className="input-group-text" id="basic-addon2">.zendesk.com</span>
-                  </div>
-                </div>
-
-                <label htmlFor="basic-url">Manager Email</label>
-                <div className="input-group mb-3">
-                  <input type="text" className="form-control" placeholder="someone" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                  <div className="input-group-append">
-                    <span className="input-group-text" id="basic-addon2">@partnerhero.com</span>
-                  </div>
-                </div>
+          <TextInput title="Program Name" placeholder="Acme corp" aria-label="Username" aria-describedby="basic-addon1"/>
+          <TextInput title="Zendesk URL" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2" prepend="https://" append=".zendesk.com"/>
+          <TextInput title="Manager Email" placeholder="someone" aria-label="Recipient's username" aria-describedby="basic-addon2" append="@partnerhero.com"/>
 			</div>
 		)
 	}
-
 }
