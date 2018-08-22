76efd81e4e7e60aba9636b8e8fbf3e03a3e6ae50
Andres Umanzor <andresumanzor@JuliettesiPhone.localdomain>
2018-08-22 09:05:29 -0600

    temp

--- src/components/teamLinegraphRenderer.js
+++ src/components/teamLinegraphRenderer.js
@@ -22,12 +22,12 @@ export default class TeamLinegraphRenderer extends Component {
         return (
                 <div style={{ marginBottom:'40px' }}>
                     <div className="row" style={{ marginBottom: '30px' }}>
-                        <CardComponent marginLeft='30px' size="col-lg-12" title="Hourly throughput" description="Actual vs expected performance" body="these are some stats" >
+                        <CardComponent size="col-lg-12" title="Hourly throughput" description="Actual vs expected performance" body="these are some stats" >
                             <Linegraph width="1000" height="280" renderData={ this.renderData(teamGraphData, globalDate) } />
                         </CardComponent>
                     </div>
                     <div className="row" style={{ marginBottom: '20px' }}>
-                        <CardComponent marginLeft='30px' size="col-lg-12" title="Queue volume" description="Today's incoming ticket volume" body="these are some stats" >
+                        <CardComponent size="col-lg-12" title="Queue volume" description="Today's incoming ticket volume" body="these are some stats" >
                             <Linegraph  width="1000" height="280" renderData={ this.renderData(queueData, globalDate, true) } />
                         </CardComponent>
                     </div>
