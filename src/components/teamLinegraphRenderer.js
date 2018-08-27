006a1d28654ab5bde0de179afa656b0ed257bbf5
Daniel Lesage <d_lesage3@hotmail.com>
2018-08-27 07:32:16 -0600

    adding style to specific canvas class

--- src/components/teamLinegraphRenderer.js
+++ src/components/teamLinegraphRenderer.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import CardComponent from './cardComponent'
 import Linegraph from './linegraph'
 
+import Radargraph from './radargraph'
+
+import dataTypes from './dataTypes'
+
 export default class TeamLinegraphRenderer extends Component {
     constructor(props){
         super(props);
@@ -27,9 +31,12 @@ export default class TeamLinegraphRenderer extends Component {
                             <Linegraph width="1000" height="280" renderData={ this.renderData(teamGraphData, globalDate) } />
                         </CardComponent>
                     </div>
-                    <div className="row" style={{ marginBottom: '20px' }} id="queueVolumeChart">
-                        <CardComponent columnClassname="col-lg-12" title="Queue volume" description="Today's incoming ticket volume" body="these are some stats">
+                    <div className="row" style={{ marginBottom: '20px' }}>
+                        <CardComponent maxWidth="45%" columnClassname="col-lg-6 individualLineGraph" title="Queue volume" description="Today's incoming ticket volume" body="these are some stats">
                             <Linegraph  width="1000" height="280" renderData={ this.renderData(queueData, globalDate, true) } />
+                        </CardComponent>
+                        <CardComponent maxWidth="45%" columnClassname="col-lg-6" title={"Tag distribution"} description="Today's production" body="these are some stats">
+                            <Radargraph  width="500" height="280" />
                         </CardComponent>
                     </div>
                 </div>
