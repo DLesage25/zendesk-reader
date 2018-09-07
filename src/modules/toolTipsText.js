0b4cc74d8166e7a119593bf0694c47973ff48f90
Daniel Lesage <d_lesage3@hotmail.com>
2018-09-07 14:52:00 -0600

    fixing description

--- src/modules/toolTipsText.js
+++ src/modules/toolTipsText.js
@@ -1,6 +1,6 @@
 module.exports = {
-    zendeskURL: "The URL for your Zendesk instance",
-    managerEmail: "The program manager's email address (has to be of the @partnergero domain)",
+    zendeskURL: "Sets the URL for your Zendesk instance.",
+    managerEmail: "The program manager's email address (has to be within the @partnergero domain)",
     goalType: "Sets the program's goal type. This will affect the goal series of all graphs in the Live Stats page.",
     olarkChats: "If true, solved tickets with the olark_chat tag will be counted as a separate ticket status."
 }
