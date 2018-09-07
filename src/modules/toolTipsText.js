58d7fb52e602e7be4d9e84dc8f104a6ae567e8a7
Daniel Lesage <d_lesage3@hotmail.com>
2018-09-07 14:51:02 -0600

    modifying help tip descriptions

--- src/modules/toolTipsText.js
+++ src/modules/toolTipsText.js
@@ -1,6 +1,6 @@
 module.exports = {
-    zendeskURL: "This program's root Zendesk URL",
-    managerEmail: "This program's manager email",
-    goalType: "Determines this program's goal type",
-    olarkChats: "Determines if this program depends on olark chats"
+    zendeskURL: "The URL for your Zendesk instance",
+    managerEmail: "The program manager's email address (has to be of the @partnergero domain)",
+    goalType: "Sets the program's goal type. This will affect the goal series of all graphs in the Live Stats page.",
+    olarkChats: "If true, solved tickets with the olark_chat tag will be counted as a separate ticket status."
 }
